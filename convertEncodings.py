3551fc1046c1759b62bfaa359b6f995c89790bbe
Axel Eschholz <axelion737@gmail.com>
2024-04-03 01:03:42 -0300

    table?

--- convertEncodings.py
+++ convertEncodings.py
@@ -17,11 +17,11 @@ for key, values in encodings.items():
     max_lengths = [max(len(f'[{i+j}] {value}') for i in range(0, len(values), num_columns)
                        for j, value in enumerate(values[i:i+num_columns])) for _ in range(num_columns)]
 
-    # Add the values as a table
+    md_content += '| ' + ' | '.join('---' for _ in range(num_columns)) + ' |\n'
     for i in range(0, len(values), num_columns):
         row_values = values[i:i+num_columns]
-        md_content += '   '.join(f'[{i+j}] {value}'.ljust(
-            max_lengths[j]) for j, value in enumerate(row_values)) + '\n'
+        md_content += '|' + '|'.join(f'[{i+j}] {value}'.ljust(
+            max_lengths[j]) for j, value in enumerate(row_values)) + '|\n'
         md_content += '\n'
 
 # Write the Markdown content to a file
