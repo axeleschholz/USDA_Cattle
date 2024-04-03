859dbcb0e1101e331c6fe8bab079165e72cc081a
Axel Eschholz <axelion737@gmail.com>
2024-04-03 01:08:38 -0300

    table?

--- convertEncodings.py
+++ convertEncodings.py
@@ -14,16 +14,16 @@ num_columns = 3
 for key, values in encodings.items():
     # Add the key as a header
     md_content += f'## {key}\n'
-    max_lengths = [max(len(f'[{i+j}] {value}') for i in range(0, len(values), num_columns)
-                       for j, value in enumerate(values[i:i+num_columns])) for _ in range(num_columns)]
+    md_content += '\n'.join(f'[{i}] {value}' for i, value in enumerate(values))
+    md_content += '\n\n'
+    # max_lengths = [max(len(f'[{i+j}] {value}') for i in range(0, len(values), num_columns)
+    #                    for j, value in enumerate(values[i:i+num_columns])) for _ in range(num_columns)]
 
-    md_content += '| ' + ' | '.join('---' for _ in range(num_columns)) + ' |\n'
-    md_content += '| ' + ' | '.join('---' for _ in range(num_columns)) + ' |\n'
-    for i in range(0, len(values), num_columns):
-        row_values = values[i:i+num_columns]
-        md_content += '| ' + ' | '.join(f'[{i+j}] {value}'.ljust(
-            max_lengths[j]) for j, value in enumerate(row_values)) + ' |\n'
-        md_content += '\n'
+    # for i in range(0, len(values), num_columns):
+    #     row_values = values[i:i+num_columns]
+    #     md_content += '| ' + ' | '.join(f'[{i+j}] {value}'.ljust(
+    #         max_lengths[j]) for j, value in enumerate(row_values)) + ' |\n'
+    #     md_content += '\n'
 
 # Write the Markdown content to a file
 with open('encodings.md', 'w') as f:
