4ddfb7658dbd1b176914b933f0363a14da2d2bcb
Axel Eschholz <axelion737@gmail.com>
2024-04-03 00:58:27 -0300

    minor encodings aesthetic

--- convertEncodings.py
+++ convertEncodings.py
@@ -14,13 +14,13 @@ num_columns = 3
 for key, values in encodings.items():
     # Add the key as a header
     md_content += f'## {key}\n'
-    max_lengths = [max(len(f'{i+j}: {value}') for i in range(0, len(values), num_columns)
+    max_lengths = [max(len(f'[{i+j}] {value}') for i in range(0, len(values), num_columns)
                        for j, value in enumerate(values[i:i+num_columns])) for _ in range(num_columns)]
 
     # Add the values as a table
     for i in range(0, len(values), num_columns):
         row_values = values[i:i+num_columns]
-        md_content += '   '.join(f'{i+j}: {value}'.ljust(
+        md_content += '   '.join(f'[{i+j}] {value}'.ljust(
             max_lengths[j]) for j, value in enumerate(row_values)) + '\n'
         md_content += '\n'
 
